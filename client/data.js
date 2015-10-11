d26f67bb78be9948d0d844a6a3e574df39b5c85d
Dean Radcliffe <deanmisc@yahoo.com>
2015-10-10 23:08:06 -0500

    Using ViewModel is like bringing a gun to a knife fight

--- client/data.js
+++ client/data.js
@@ -6,15 +6,21 @@ ExampleRun = {
   unit: "miles",
   /* start screen fields */
   targetDistance: 10.0,
-  numberWaypoints: 20,
-  targetPace: {
-    secondsPerWaypoint: 270
-  },
+  numberOfWaypoints: 20,
+  secondsPerWaypoint: 270,
+  distancePerWaypoint: 0.5,
+  startedAt: new Date(2015, 10, 10, 16, 0, 0),
+  completedAt: null,
   /* running screen fields */
   passedWaypoints: [
     {distance: 0.5, intervalTime: 240, surplus: 30},
     {distance: 0.5, intervalTime: 290, surplus: 10}
   ],
-  elapsedTime: 530,
-  currentSurplus: 40
+  elapsedTime: 530
 }
+
+Runs = new Mongo.Collection(null);
+Runs.remove({});
+Runs.insert(ExampleRun)
+
+theRun = () => Runs.findOne()
